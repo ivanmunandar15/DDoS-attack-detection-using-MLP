2a93320dbb2e38d6146a06120e4efad74eb64e40
Ivan Munandar <3332190035@untirta.ac.id>
2023-09-06 23:11:35 +0700

    ubah topo.py

--- topologi.py
+++ topologi.py
@@ -7,26 +7,23 @@ from mininet.node import OVSKernelSwitch, RemoteController
 
 class MyTopo(Topo):
     def build(self):
-
-        s = []
-
-        # Add switches
-        for i in range(1, 6):
-            switch = self.addSwitch(f's{i}', cls=OVSKernelSwitch, protocols='OpenFlow13')
-            s.append(switch)
-
-        # Add hosts
+        switches = []
         hosts = []
-        for i in range(1, 16):
-            host = self.addHost(f'h{i}', cpu=1.0/20, mac=f"00:00:00:00:00:{i:02}", ip=f"10.0.0.{i}/24")
-            hosts.append(host)
 
-        # Add links
-        for i in range(0, 15):
-            self.addLink(hosts[i], s[i // 3])
+        for s in range(1, 6):
+            switch_name = 's{}'.format(s)
+            switch = self.addSwitch(switch_name, cls=OVSKernelSwitch, protocols='OpenFlow13')
+            switches.append(switch)
 
-        for i in range(0, 4):
-            self.addLink(s[i], s[i + 1])
+            for h in range(1, 4):
+                host_name = 'h{}'.format(s * 3 + h - 3)
+                host_ip = '10.0.0.{}/24'.format(s * 3 + h - 3)
+                host = self.addHost(host_name, cpu=1.0/20, mac="00:00:00:00:00:{:02d}".format(s * 3 + h - 3), ip=host_ip)
+                hosts.append(host)
+                self.addLink(host, switch)
+
+        for i in range(len(switches) - 1):
+            self.addLink(switches[i], switches[i + 1])
 
 def startNetwork():
     topo = MyTopo()
