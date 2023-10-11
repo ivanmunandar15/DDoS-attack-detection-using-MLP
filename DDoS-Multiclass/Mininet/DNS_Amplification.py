73c1f439ae74e46cf0f46482f4025b384f0f3ab1
ivan munandar <3332190035@untirta.ac.id>
2023-10-12 00:23:12 +0700

    ubah dataset menjadi 5 kelas yang merepresentasikan serangan DDoS

---
+++ DDoS-Multiclass/Mininet/DNS_Amplification.py
@@ -0,0 +1,66 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.log import setLogLevel
+from mininet.node import OVSKernelSwitch, RemoteController
+from time import sleep
+from datetime import datetime
+from random import randrange, choice
+
+class MyTopo(Topo):
+    def build(self):
+        switches = []
+        hosts = []
+
+        for s in range(1, 6):
+            switch_name = 's{}'.format(s)
+            switch = self.addSwitch(switch_name, cls=OVSKernelSwitch, protocols='OpenFlow13')
+            switches.append(switch)
+
+            for h in range(1, 4):
+                host_name = 'h{}'.format(s * 3 + h - 3)
+                host_ip = '10.0.0.{}/24'.format(s * 3 + h - 3)
+                host = self.addHost(host_name, cpu=1.0/20, mac="00:00:00:00:00:{:02d}".format(s * 3 + h - 3), ip=host_ip)
+                hosts.append(host)
+                self.addLink(host, switch)
+
+        for i in range(len(switches) - 1):
+            self.addLink(switches[i], switches[i + 1])
+
+def ip_generator():
+    ip = ".".join(["10", "0", "0", str(randrange(1, 15))])
+    return ip
+
+def startNetwork():
+    topo = MyTopo()
+    c0 = RemoteController('c0', ip='127.0.0.1')
+    net = Mininet(topo=topo, link=TCLink, controller=c0)
+    net.start()
+
+    h1 = net.get('h1')
+    hosts = [h1] + [net.get(f'h{i}') for i in range(2, 15)]
+
+    h1.cmd('cd /home/mininet/webserver')
+    h1.cmd('python -m SimpleHTTPServer 80 &')
+    
+    src = choice(hosts)
+    dst = ip_generator()   
+    print("--------------------------------------------------------------------------------")
+    print("Performing DNS Amplification")  
+    print("--------------------------------------------------------------------------------")   
+    src.cmd("timeout 20s hping3 --rand-source -p 53 --flood --data 100 {}".format(dst,dst))
+    sleep(100)  
+    print("--------------------------------------------------------------------------------")
+
+    # CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    start = datetime.now()
+
+    setLogLevel('info')
+    startNetwork()
+
+    end = datetime.now()
+
+    print(end - start)
---
+++ DDoS-Multiclass/Ryu-Manager/ICMP_kontroler.py
@@ -0,0 +1,123 @@
+import topologi_kontroler
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.lib import hub
+import os
+import datetime
+
+
+class CollectFlowStatsApp(topologi_kontroler.SimpleSwitch13):
+    def __init__(self, *args, **kwargs):
+        super(CollectFlowStatsApp, self).__init__(*args, **kwargs)
+        self.datapaths = {}
+        self.monitor_thread = hub.spawn(self.monitor)
+
+    @set_ev_cls(ofp_event.EventOFPStateChange, [CONFIG_DISPATCHER, MAIN_DISPATCHER])
+    def state_change_handler(self, ev):
+        datapath = ev.datapath
+        if ev.state == MAIN_DISPATCHER:
+            if datapath.id not in self.datapaths:
+                self.datapaths[datapath.id] = datapath
+                self.logger.debug('Registered datapath: %016x', datapath.id)
+
+        elif ev.state == CONFIG_DISPATCHER:
+            if datapath.id in self.datapaths:
+                del self.datapaths[datapath.id]
+                self.logger.debug('Unregistered datapath: %016x', datapath.id)
+
+    def monitor(self):
+        while True:
+            for datapath in self.datapaths.values():
+                self.request_stats(datapath)
+            hub.sleep(10)
+
+    def request_stats(self, datapath):
+        self.logger.debug('Sending stats request to datapath: %016x', datapath.id)
+        parser = datapath.ofproto_parser
+        req = parser.OFPFlowStatsRequest(datapath)
+        datapath.send_msg(req)
+
+    # Fungsi untuk menangani balasan statistik aliran protokol OpenFlow
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def handle_flow_stats_reply(self, event):
+        # Mendapatkan stempel waktu saat ini
+        timestamp = datetime.now().timestamp()
+
+        # Variabel untuk informasi ICMP, TCP, dan UDP
+        icmp_code = -1
+        icmp_type = -1
+        tp_src = 0
+        tp_dst = 0
+
+        # Memastikan folder dataset ada, jika tidak, buat folder tersebut
+        dataset_folder = 'dataset'
+        os.makedirs(dataset_folder, exist_ok=True)
+
+        # Path file CSV di dalam folder dataset untuk menyimpan statistik aliran
+        file_path = os.path.join(dataset_folder, "FlowStatsfile.csv")
+
+        # Menyertakan header jika file belum ada atau kosong
+        header = "Timestamp,DatapathID,FlowID,IPSource,PortSource,IPDestination,PortDestination,IPProtocol," \
+                "ICMPCode,ICMPType,DurationSecond,DurationNSecond,IdleTimeout,HardTimeout,Flags," \
+                "PacketCount,ByteCount,PacketCountPerSecond,PacketCountPerNSecond,ByteCountPerSecond," \
+                "ByteCountPerNSecond,Label\n"
+
+        # Membuka atau membuat file CSV di dalam folder dataset untuk menyimpan statistik aliran
+        with open(file_path, "a+") as file:
+            # Menyertakan header jika file kosong
+            if os.path.getsize(file_path) == 0:
+                file.write(header)
+
+            # Ekstrak statistik aliran dari pesan acara
+            body = event.msg.body
+            
+            # Menelusuri aliran dengan prioritas 1 dan mengurutkannya berdasarkan kriteria tertentu
+            for stat in sorted([flow for flow in body if flow.priority == 1], key=lambda flow: (
+                    flow.match['eth_type'], flow.match['ipv4_src'], flow.match['ipv4_dst'], flow.match['ip_proto'])):                    
+                           
+                ip_src = stat.match['ipv4_src']
+                ip_dst = stat.match['ipv4_dst']
+                ip_proto = stat.match['ip_proto']
+
+                # Menentukan detail ICMP, TCP, atau UDP berdasarkan protokol IP
+                if ip_proto == 1:
+                    icmp_code = stat.match['icmpv4_code']
+                    icmp_type = stat.match['icmpv4_type']
+                elif ip_proto == 6:
+                    tp_src = stat.match['tcp_src']
+                    tp_dst = stat.match['tcp_dst']
+                elif ip_proto == 17:
+                    tp_src = stat.match['udp_src']
+                    tp_dst = stat.match['udp_dst']
+
+                # Menghasilkan ID aliran unik
+                flow_id = str(ip_src) + str(tp_src) + str(ip_dst) + str(tp_dst) + str(ip_proto)
+
+                # Menghitung jumlah paket dan byte per detik dan per nanodetik
+                try:
+                    packet_count_per_second = stat.packet_count / stat.duration_sec
+                    packet_count_per_nsecond = stat.packet_count / stat.duration_nsec
+                except:
+                    packet_count_per_second = 0
+                    packet_count_per_nsecond = 0
+
+                try:
+                    byte_count_per_second = stat.byte_count / stat.duration_sec
+                    byte_count_per_nsecond = stat.byte_count / stat.duration_nsec
+                except:
+                    byte_count_per_second = 0
+                byte_count_per_nsecond = 0
+
+                # Menulis statistik aliran ke file CSV di dalam folder dataset
+                file.write("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(
+                    timestamp, event.msg.datapath.id, flow_id, ip_src, tp_src, ip_dst, tp_dst,
+                    ip_proto, icmp_code, icmp_type, stat.duration_sec, stat.duration_nsec,
+                    stat.idle_timeout, stat.hard_timeout, stat.flags, stat.packet_count, stat.byte_count,
+                    packet_count_per_second, packet_count_per_nsecond, byte_count_per_second, byte_count_per_nsecond, 1))
+                
+            file.close()
+
+if __name__ == "__main__":
+    from ryu.cmd import manager
+    manager.main()
